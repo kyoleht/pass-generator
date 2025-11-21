7fb35ead5d738ca64a68e834c4a23b3d1bcc880f
kyoleht <rafaelferreiradasilva1812@gmail.com>
2025-11-20 23:53:41 -0300

    resolvendo bug do botão

--- demo/src/main/java/com/example/PassGenerator.java
+++ demo/src/main/java/com/example/PassGenerator.java
@@ -3,9 +3,14 @@ package com.example;
 import java.security.SecureRandom;
 
 import javafx.application.Application;
+import javafx.geometry.Insets;
+import javafx.geometry.Pos;
+import javafx.scene.Scene;
 import javafx.scene.control.Alert;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 
 
@@ -28,24 +33,38 @@ public class PassGenerator extends Application {
     @Override
     public void start(Stage palco) {
         Label passLabel = new Label("Password");
+        //numero que o usuario digita
+        TextField lengthPassField = new TextField();
+        lengthPassField.setPromptText("Enter password length");
         Button genButton = new Button("Generate Password");
         genButton.setOnAction (e -> {
             try {
                 // pega o valor e atualiza o texto
-                String password = generatePass(12);
+                int length = Integer.parseInt(lengthPassField.getText());
+                // pasando o valor para gerar a senha
+                // estava passando TextField, que é um objeto do javafx 
+                String password = generatePass(length);
                 passLabel.setText(password);
-            } catch (ArrayIndexOutOfBoundsException aiobException) {
+            } catch (NumberFormatException nfException) {
                 Alert alert = new Alert(Alert.AlertType.ERROR);
                 alert.setTitle("Error");
                 alert.setHeaderText("An error occurred while generating the password.");
-                alert.setContentText("Please try again. " + aiobException.getMessage());
+                alert.setContentText("Please try again. " + nfException.getMessage());
                 alert.showAndWait();
             }
         });
 
-        
-    }
+        VBox layout = new VBox(10, lengthPassField, genButton, passLabel);
 
+        layout.setPadding(new Insets(10));
+        layout.setAlignment(Pos.CENTER);
+
+        Scene scene = new Scene(layout, 450, 450);
+        palco.setScene(scene);
+        palco.setTitle("Password Generator");
+        palco.show();
+    }
+    
     public static void main(String[] args) {
         launch();
     }
